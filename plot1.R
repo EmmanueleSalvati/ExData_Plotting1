8eb39e0413901fb75c6ebea0fdb9d2ac2409cf9c
Emmanuele <emmanuele.salvati@gmail.com>
2014-09-06 20:26:22 -0400

    Complete plot2 and fix axis label size

--- plot1.R
+++ plot1.R
@@ -3,6 +3,7 @@ df <- read.table(file = "dataset/household_power_consumption.txt",
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df <- df[df$Date == "2007-02-01" | df$Date == "2007-02-02", ]
 hist(df$Global_active_power, xlab = "Global Active Power (kilowatts)",
-     main = "Global Active Power", col = "red")
+     main = "Global Active Power", col = "red",
+     cex.lab = 0.75, cex.axis = 0.75)
 dev.copy(png, "figure/plot1.png")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,7 +2,15 @@ df <- read.table(file = "dataset/household_power_consumption.txt",
                  header = TRUE, sep = ";", na.strings = "?")
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df <- df[df$Date == "2007-02-01" | df$Date == "2007-02-02", ]
-with(df, {plot(Time, Global_active_power)})
 
-## I think I need to use the datetime functions to combine the Date and Time
-## columns
+## Combine the Date and Time into a single DateTime column
+df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format = "%Y-%m-%d %H:%M:%S")
+
+with(df, {
+        plot(DateTime, Global_active_power, type = "l", xlab = "",
+             ylab = "Global Active Power (kilowatts)",
+             cex.lab = 0.75, cex.axis = 0.75)
+        })
+
+dev.copy(png, "figure/plot2.png")
+dev.off()
